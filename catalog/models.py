8214822554fc0dd3adad67d827466868c136c330
AlexanderPshenichnyy <ter.anti@mail.ru>
2023-11-19 18:51:51 +0300

    migrate

--- catalog/models.py
+++ catalog/models.py
@@ -1,21 +1,43 @@
 from django.db import models
 
 
+# #
+#     наименование,
+#     описание,
+#     изображение (превью),
+#     категория,
+#     цена за штуку,
+#     дата создания,
+#     дата последнего изменения.
+#
+# Category:
+#
+#     наименование,
+#     описание.
+
 class Product(models.Model):
-    pass
+    title = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=255, verbose_name='описание')
+    image = models.ImageField(verbose_name='изображение')
+    catgory = models.CharField(max_length=100, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за штуку')
+    date_of_creation = models.DateField(auto_now=True, verbose_name='дата создания')
+    last_modified_date = models.DateTimeField(verbose_name='дата изменения')
 
     def __str__(self):
-        pass
-
-    class Meta:
-        pass
+        return f'{self.title}' \
+               f'{self.description}' \
+               f'{self.catgory}' \
+               f'{self.price}' \
+               f'{self.date_of_creation}' \
+               f'{self.last_modified_date}'
 
 
 class Category(models.Model):
-    pass
+    title = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=255, verbose_name='описание')
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        pass
-
-    class Meta:
-        pass
+        return (f'{self.title}'
+                f'{self.description}')
