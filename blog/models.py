0ee5cdeb10f790b04832cba5446a6726479ea605
AlexanderPshenichnyy <ter.anti@mail.ru>
2023-12-01 22:15:07 +0300

    21.1 Done.

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,9 @@ class Blog(models.Model):
     slug = models.CharField(max_length=255)
     content = models.TextField(verbose_name='Cодержимое')
     preview = models.ImageField(upload_to='preview/', verbose_name='Превью статьи')
-    date_of_creation = models.DateTimeField(verbose_name='Дата создания')
-    is_published = models.BooleanField(verbose_name='Признак публикации')
-    count_of_view = models.IntegerField(verbose_name='Количество просмотров')
+    date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Признак публикации')
+    count_of_view = models.IntegerField(default=0, verbose_name='Количество просмотров')
 
     def __str__(self):
         return f'{self.title}'
--- blog/urls.py
+++ blog/urls.py
@@ -3,15 +3,16 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
+    path('', BlogListView.as_view(), name='blog'),
     path('create/', BlogCreateView.as_view(), name='create'),
-    # path('view/', ..., name='view'),
-    # path('edit/', ..., name='edit'),
-    # path('delete/', ..., name='delete'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='view'),
+    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
 ]
 
 if settings.DEBUG:
